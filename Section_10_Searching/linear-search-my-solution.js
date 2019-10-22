91be5d6f3e605d404e0a343bc789012b454c7737
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-22 17:25:28 +0800

    Complete Linear Search exercise

---
+++ Section_10_Searching/linear-search-my-solution.js
@@ -0,0 +1,13 @@
+function linearSearch(arr, val) {
+  if (arr.length < 1) {
+    return -1
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i
+    }
+  }
+
+  return -1
+}
