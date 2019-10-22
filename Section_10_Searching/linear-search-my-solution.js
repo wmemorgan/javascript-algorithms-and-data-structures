f3122dd9c28442802548d3ba7ce61e3e65e1ee4c
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-10-22 17:27:29 +0800

    Shorten code

--- Section_10_Searching/linear-search-my-solution.js
+++ Section_10_Searching/linear-search-my-solution.js
@@ -1,12 +1,8 @@
 function linearSearch(arr, val) {
-  if (arr.length < 1) {
-    return -1
-  }
+  if (arr.length < 1) return -1
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === val) {
-      return i
-    }
+    if (arr[i] === val) return i 
   }
 
   return -1
