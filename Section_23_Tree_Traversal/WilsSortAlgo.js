4c651666eb875e1ea067c896f194c56ff917e7ee
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-27 14:02:20 +0800

    Complete Depth first search lecture

---
+++ Section_23_Tree_Traversal/WilsSortAlgo.js
@@ -0,0 +1,160 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    // Edge case check
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (value == current.value) return undefined;
+
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(value) {
+    if (!this.root) return false;
+
+    let currentNode = this.root;
+    let found = false;
+    while (currentNode && !found) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        found = true;
+      }
+    }
+
+    // Return the value or undefined if not found
+    if (!found) return undefined;
+    return currentNode;
+  }
+
+  contains(value) {
+    if (!this.root) return false;
+
+    let currentNode = this.root;
+    let found = false;
+    while (currentNode && !found) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return (found = true);
+      }
+    }
+
+    return found;
+  }
+
+  BFS() {
+    if (!this.root) return undefined
+
+    let node = this.root
+    const queue = []
+    const visited = []
+
+    queue.push(node)
+
+    while (queue.length > 0) {
+      node = queue.shift()
+      visited.push(node.value)
+
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+
+    return visited
+  }
+
+  DFSPreOrder() {
+    if (!this.root) return undefined
+
+    const visited = []
+    const current = this.root
+
+    // Base case
+    traverse(current)
+    return visited
+
+    function traverse(node) {
+      visited.push(node.value)
+
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+    }
+  }
+
+  DFSPostOrder() {
+    if (!this.root) return undefined
+
+    const visited = []
+    const current = this.root
+    traverse(current)
+    return visited
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      if (node.right) traverse(node.right)
+
+      visited.push(node.value)
+    }
+  }
+
+  DFSInOrder() {
+    if (!this.root) return undefined
+
+    const visited = []
+    const current = this.root
+    traverse(current)
+    return visited
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      visited.push(node.value)
+      if (node.right) traverse(node.right)
+    }
+  }
+}
+
+function wilsSearch(arr) {
+  const tree = new BinarySearchTree()
+  for (let el of arr) {
+    tree.insert(el)
+  }
+
+  return tree.DFSInOrder()
+}
+
+const myArr = [453, 6, 2, 9, 6, 43, 89, 23, 4, 0]
+console.log(wilsSearch(myArr))
